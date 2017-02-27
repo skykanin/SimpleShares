e6534bfdbc3f6c1d7fe2a4b03e0ab57228779013
skykanin <nicholas.jaunsen@gmail.com>
2017-02-27 01:57:40 +0100

    Fixed cancer CSS for div elements on front page

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 var request = require('request');
+var stockList = ["goog", "aapl", "ssnlf", "test0", "test1", "test2"];
 
-function googleRequest(){
-	request("https://www.google.com/finance/info?client=ig&q=goog", function(error, response, body) {
-		body = body.slice(3);
-		body = JSON.parse(body);
-		console.log(body);
-		newPrice(body);
+function getInfo(stock){
+	var url = "https://www.google.com/finance/info?client=ig&q=" + stock;
+	console.log(url);
+	request(url, function(error, response, body) {
+		console.log('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		if(!error && response.statusCode === 200) {
+			body = body.slice(3);
+			body = JSON.parse(body);
+			console.log(body);
+			draw(body, stock);
+		}
 	});
-	function newPrice(arr) {
-		currentPrice = arr[0]["l"];
-		document.getElementsByClassName("stocks")[0].innerHTML = currentPrice;
-	}
-}
 
-setInterval(googleRequest(), 2000);
+	function draw(arr, stock){
+		currentPrice = arr[0]["l"];
+		var caption = document.createElement("caption");
+		var text = document.createTextNode(stock.toUpperCase());
+		var span = document.createElement("span");
+		var value = document.createTextNode(currentPrice + "$");
+		var div = document.createElement("div");
+		var stockDiv = document.getElementsByClassName("stocks")[stockList.indexOf(stock)];
+		stockDiv.appendChild(div);
+		span.appendChild(value);
+		div.appendChild(span);
+		div.style.height = (currentPrice/5).toString() + "px";
+		div.className = "bar";
+		}
+	}
+
+
+/*for(var i = 0; i < stockList.length; i++){
+	getInfo(stockList[i]);
+}*/
+
+getInfo(stockList[0]);
+getInfo(stockList[1]);
