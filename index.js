5087818ba1892d7493b49c16125d5c0274064906
skykanin <nicholas.jaunsen@gmail.com>
2017-05-24 20:35:43 +0200

    Added flip animation
    Added animation and changed naming function to front side of stock cards
    only

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var stockNames = ["Alphabet Inc.", "Apple Inc.", "Amazon Inc.", "IBM Inc.", "Tes
 
 function getInfo(stock){
 	var url = "https://www.google.com/finance/info?client=ig&q=" + stock;
-	console.log(url);
+	//console.log(url);
 	request(url, function(error, response, body) {
 		//console.log('error:', error); // Print the error if one occurred
 		//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -12,60 +12,103 @@ function getInfo(stock){
 			body = body.slice(3);
 			body = JSON.parse(body);
 			//console.log(body);
-			draw(body, stock);
+			drawTable(body, stock);
 		}
 	});
+}
 
-	function draw(arr, stock){
-		var infoNames = ["Ticker", "Listing Price", "Exchange", "Growth"];
-		var info = [arr[0]["t"], arr[0]["l"], arr[0]["e"], arr[0]["c"]];
-		var text = document.createTextNode(stock.toUpperCase());
-		var stockDiv = document.getElementsByClassName("stocks")[stockList.indexOf(stock)];
+function drawTable(arr, stock){
+	var infoNames = ["Ticker", "Listing Price", "Exchange", "Growth"];
+	var info = [arr[0]["t"], arr[0]["l"], arr[0]["e"], arr[0]["c"]];
+	var text = document.createTextNode(stock.toUpperCase());
+	var stockDiv = document.getElementsByClassName("stocks")[stockList.indexOf(stock)];
 
-		var table = stockDiv.children[1].firstChild;
-		//console.log("Table " + table);
-		//Creates all table rows and appends them to table
-		for (var x = 0; x < info.length; x++) {
-			var tr = document.createElement("tr");
-			table.insertBefore(tr, table.children[i]);
-		}
-		//Creats all table data and appends them to table rows
-		for (var i = 0; i < info.length; i++) {
-			var tr = table.children[i];
-			var td0 = document.createElement("td");
-			var text0 = document.createTextNode(infoNames[i]);
-			var td1 = document.createElement("td");
-			var text1 = document.createTextNode(info[i]);
+	var table = stockDiv.getElementsByTagName("table")[0];
+	//console.log("Table " + table);
+	//Creates all table rows and appends them to table
+	for (var x = 0; x < info.length; x++) {
+		var tr = document.createElement("tr");
+		table.insertBefore(tr, table.children[i]);
+	}
+	//Creats all table data and appends them to table rows
+	for (var i = 0; i < info.length; i++) {
+		var tr = table.children[i];
+		var td0 = document.createElement("td");
+		var text0 = document.createTextNode(infoNames[i]);
+		var td1 = document.createElement("td");
+		var text1 = document.createTextNode(info[i]);
 
-			td0.appendChild(text0);
-			tr.appendChild(td0);
-			td1.appendChild(text1);
-			tr.appendChild(td1);
-		}
+		td0.appendChild(text0);
+		tr.appendChild(td0);
+		td1.appendChild(text1);
+		tr.appendChild(td1);
+	}
 
-		var td = table.children[3].lastChild;
-		//console.log(td);
+	var td = table.children[3].lastChild;
+	//console.log(td);
 
-		if (td.innerHTML.charAt(0) === "+") {
-			td.style.color = "#00FF00";
-		} else {
-			td.style.color = "#DD0048";
-		}
+	if (td.innerHTML.charAt(0) === "+") {
+		td.style.color = "#00FF00";
+	} else {
+		td.style.color = "#DD0048";
+	}
+}
 
-		//Adds names to all the stocks
-		var head = document.getElementsByClassName("head");
-		for (var y = 0; y < stockNames.length; y++) {
+function drawGraph(){
+	//TODO: this
+}
+
+function setupClickHandlers(){
+    var stocks = document.getElementsByClassName("stocks");
+    for (var i = 0; i < stocks.length; i++) {
+        var stock = stocks[i];
+        var flipper = stock.children[0];
+        stock.onclick = (function(flipper){
+			var rotated = false;
+            return function() {
+                if(!rotated){
+                    flipper.style.transform = "rotateY(180deg)";
+                    flipper.style.webkitTransform = "rotateY(180deg)";
+                    rotated = true;
+                } else if(rotated) {
+                    flipper.style.transform = "rotateY(0deg)";
+                    flipper.style.webkitTransform = "rotateY(0deg)";
+                    rotated = false;
+                }
+            };
+        })(flipper);
+    }
+}
+
+function flip(){
+	var rotated = false;
+	if(!rotated){
+		flipper.style.transform = "rotateY(180deg)";
+		flipper.style.webkitTransform = "rotateY(180deg)";
+		rotated = true;
+	} else {
+		flipper.style.transform = "rotateY(0deg)";
+		flipper.style.webkitTransform = "rotateY(0deg)";
+		rotated = false;
+	}
+}
+
+//Adds names to all the stocks
+function namer(){
+	var head = document.getElementsByClassName("head");
+	for (var y = 0; y < stockNames.length; y++) {
+		if(!head[y].hasChildNodes()){
 			var node = document.createTextNode(stockNames[y]);
 			var h1 = document.createElement("h1");
-			if(!head[y].hasChildNodes()){
-				head[y].appendChild(h1);
-				h1.appendChild(node);
-			}
+			h1.appendChild(node);
+			head[y].appendChild(h1);
 		}
 	}
 }
 
-//Runs the function for displaying the front page
+//Runs the functions for displaying the front page
 for(var i = 0; i < stockList.length; i++){
 	getInfo(stockList[i]);
 }
+namer();
+setupClickHandlers();
